10008ec2d58e1465acb24bd2733dda9da47134da
Joe Mattelaer <josephmattelaer@gmail.com>
2014-05-23 16:27:14 +0100

    Initial Setup
    This commit puts in place the work done so far on the project.
    
    Features Added:
    - Basic player movement
    - When the player clicks, a preview of what they are about to place
    appears, when they click again this preview solidifies into a game
    object
    
    Bugs/Errors:
    - The preview wall is not central to the player's camera
    - The preview wall's Y rotation is not locked, and therefore the wall
    can be placed at an angle so, when it solidifies it will fall apart
    instantly

---
+++ Project/Assets/BuildWall.js
@@ -0,0 +1,62 @@
+﻿#pragma strict
+
+var InvisWall : GameObject;
+var Wall : GameObject;
+
+var Placing : boolean = false;
+
+var PrePlaceWall : GameObject;
+
+function Start () {
+
+
+
+}
+
+function Update () {
+
+	if(Input.GetMouseButtonDown(0)){ // If left click down
+	
+		if(Placing == false){ // If there is not currently a wall be previeewed
+		
+			// There is now a wall being previewed
+			Placing = true;
+			
+			// Define a variable for the preview
+			PrePlaceWall = Instantiate(InvisWall, transform.position + transform.forward * 10, transform.rotation);
+			
+			// Make sure that the preview wall is a child of the camera so it moves with the camera
+			PrePlaceWall.transform.parent = transform;
+			
+			if(PrePlaceWall != null){
+				Debug.Log("After placing wall, PrePlaceWall is not null!");
+			}
+			
+			
+		}else{ // If there is currently a wall being previewed
+		
+			if(PrePlaceWall != null){
+			
+				Debug.Log("PrePlaceWall not null");
+				
+				// There is no longer a wall being previewed
+				Placing = false;
+					
+				// Place the real wall in the position of the preview wall
+				Instantiate(Wall, PrePlaceWall.transform.position, PrePlaceWall.transform.rotation);
+					
+				// Destroy the Preview wall
+				Destroy(PrePlaceWall);
+			}
+		}
+	}
+	
+	
+	// Make sure that if the PrePlaceWall exists that it has not rotated
+	if(PrePlaceWall != null){
+
+		//PrePlaceWall.transform.rotation.z = 0;
+	
+	}
+	
+}
---
+++ Project/Assets/Move.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+
+
+function Start () {
+
+	
+	var position : Vector3 = transform.position;
+
+}
+
+function Update () {
+
+	if(Input.GetKeyDown(KeyCode.Q)){
+	
+			
+		Debug.Log("Hello!");		
+			
+	
+		rigidbody.AddForce(Vector3(1000, 0, 0));
+
+		
+	}
+
+}
